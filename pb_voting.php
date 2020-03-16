1011cf0d93c695dcc23d7dd7f574833d107c9f8c
mista-git <stastny.mlslv@gmail.com>
2020-03-16 22:15:08 +0100

    Verze 1.1.8

--- pb_voting.php
+++ pb_voting.php
@@ -4,7 +4,7 @@ Plugin Name: Participativní projekty - hlasování
 Plugin URI: https://urad.online
 Description: Registrace, správa a hlasování o návrzích participativního rozpočtu. Integrace s Limesurvey a https://sms.sluzba.cz
 Author: Miloslav Stastny
-Version: 1.1.7
+Version: 1.1.8
 Author URI: https://urad.online
 Text Domain: pb-voting
 Domain Path: /languages
